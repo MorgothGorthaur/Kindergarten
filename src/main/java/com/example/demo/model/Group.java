1ebeeb07d47a89a9ff5519a2dc504cf9c172b8d5
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-05 15:53:59 +0200

    added group`s model and repository

---
+++ src/main/java/com/example/demo/model/Group.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "groups")
+@Getter @Setter
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "max_size", nullable = false)
+    private int maxSize;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Child> kids;
+}
