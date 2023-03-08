e32f84b0a43c9992e0d9880f4399275e798cc8c9
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-08 19:57:14 +0200

    refactored queries

--- src/main/java/com/example/demo/repository/ChildRepository.java
+++ src/main/java/com/example/demo/repository/ChildRepository.java
@@ -9,11 +9,11 @@ import java.util.Optional;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
 
-    @Query(value = "select children.id, children.name, children.birth_year, children.group_id from children \n" +
+    @Query(value = "select children.* from children \n" +
             "join teachers t on children.group_id = t.group_id\n" +
             "where t.email = ?1", nativeQuery = true)
     List<Child> getKidsByTeacherEmail(String email);
-    @Query(value = "select children.id, children.name, children.birth_year, children.group_id from children \n" +
+    @Query(value = "select children.* from children \n" +
             "join teachers t on children.group_id = t.group_id\n" +
             "where children.id = ?1 and t.email = ?2", nativeQuery = true)
     Optional<Child> getChildByIdAndTeacherEmail(long id, String email);
