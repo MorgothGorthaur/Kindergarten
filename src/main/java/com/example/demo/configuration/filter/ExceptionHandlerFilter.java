b78fc7d6491bdedbc7f494c5aa99399610c1b8cb
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-09 17:08:04 +0200

    added exception handlers

---
+++ src/main/java/com/example/demo/configuration/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,33 @@
+package com.example.demo.configuration.filter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (RuntimeException ex) {
+            response.setHeader("error", ex.getMessage());
+            response.setStatus(FORBIDDEN.value());
+            var error = new HashMap<String, String>();
+            error.put("error_message", ex.getMessage());
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), error);
+        }
+    }
+}
