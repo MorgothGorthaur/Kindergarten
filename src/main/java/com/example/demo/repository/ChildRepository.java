4e67963ca36dc30169b657e1c4e256ced9ed2eaf
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-14 19:39:30 +0200

    rewrite query to jpql syntax

--- src/main/java/com/example/demo/repository/ChildRepository.java
+++ src/main/java/com/example/demo/repository/ChildRepository.java
@@ -11,16 +11,16 @@ public interface ChildRepository extends JpaRepository<Child, Long> {
 
     @Query("SELECT c FROM Child c JOIN c.group.teacher t WHERE t.email = ?1")
     List<Child> findKidsByTeacherEmail(String email);
+
     @Query("SELECT c FROM Child c JOIN c.group.teacher t LEFT JOIN FETCH c.relatives WHERE t.email = ?1")
     List<Child> findKidsWithRelativesByTeacherEmail(String email);
+
     @Query("SELECT c FROM Child c JOIN c.group.teacher t WHERE c.id = ?1 AND t.email = ?2")
     Optional<Child> findChildByIdAndTeacherEmail(long id, String email);
 
-    @Query(value = """
-            SELECT children.*
-            FROM children
-            JOIN teachers ON children.group_id = teachers.group_id
-            WHERE teachers.email = ?1 and DATE_ADD(birth_year, INTERVAL YEAR(CURRENT_DATE()) - YEAR(birth_year) YEAR) > CURRENT_DATE();""", nativeQuery = true)
+    @Query("SELECT c FROM Child c JOIN c.group.teacher t WHERE t.email = ?1" +
+            " AND (MONTH(c.birthYear) > MONTH(CURRENT_DATE())" +
+            " OR (MONTH(c.birthYear) = MONTH(CURRENT_DATE()) AND DAY(c.birthYear) > DAY(CURRENT_DATE())))")
     List<Child> findChildThatWaitBirthDay(String email);
 
     @Query("SELECT c FROM Child c JOIN FETCH c.relatives r JOIN FETCH r.kids k WHERE k.id = ?1 AND c.id <> k.id")
@@ -28,7 +28,6 @@ public interface ChildRepository extends JpaRepository<Child, Long> {
 
     @Query("SELECT c FROM Child c LEFT JOIN FETCH c.relatives r JOIN c.group.teacher t WHERE c.id = ?1 AND t.email = ?2")
     Optional<Child> findChildWithRelativesByIdAndTeacherEmail(long childId, String teacherEmail);
-
 
 
 }
