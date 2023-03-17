884fff63ec06c89084fb5960e275c2ca99042129
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-17 16:17:34 +0200

    refactored: removed "magic" strings

--- src/main/java/com/example/demo/configuration/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/example/demo/configuration/filter/CustomAuthorizationFilter.java
@@ -2,6 +2,8 @@ package com.example.demo.configuration.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.example.demo.configuration.filter.enums.AuthorizationType;
+import com.example.demo.configuration.filter.enums.Claim;
 import com.example.demo.exception.BadTokenException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -26,18 +28,18 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var authorizationHeader = request.getHeader(AUTHORIZATION);
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) verifyTokens(authorizationHeader);
+        if (authorizationHeader != null && authorizationHeader.startsWith(AuthorizationType.BEARER.getPrefix())) verifyTokens(authorizationHeader);
         filterChain.doFilter(request, response);
     }
 
     private void verifyTokens(String authorizationHeader) {
         try {
-            var token = authorizationHeader.substring("Bearer ".length());
+            var token = authorizationHeader.substring(AuthorizationType.BEARER.getPrefix().length());
             var algorithm = Algorithm.HMAC256(SECRET_KEY.getBytes());
             var verifier = JWT.require(algorithm).build();
             var decoderJWT = verifier.verify(token);
             var username = decoderJWT.getSubject();
-            var roles = decoderJWT.getClaim("roles").asArray(String.class);
+            var roles = decoderJWT.getClaim(Claim.ROLES.getClaim()).asArray(String.class);
             var authorities = new ArrayList<SimpleGrantedAuthority>();
             stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
             var authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
