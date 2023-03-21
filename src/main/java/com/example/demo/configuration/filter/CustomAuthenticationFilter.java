4e7d3a2785ad71413faa1382418c622e89925888
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-21 23:57:38 +0200

    refactored

--- src/main/java/com/example/demo/configuration/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/demo/configuration/filter/CustomAuthenticationFilter.java
@@ -36,7 +36,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     private Authentication authenticate(UsernamePasswordAuthenticationToken token) {
         try {
             return authenticationManager.authenticate(token);
-
         } catch (InternalAuthenticationServiceException ex) {
             throw new BadPasswordOrEmailException(token.getName());
         }
