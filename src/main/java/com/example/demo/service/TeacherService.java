e3fa3158e4ac9523b0f64c7cdb46466fdb684911
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-24 21:14:51 +0200

    added JavaDoc

--- src/main/java/com/example/demo/service/TeacherService.java
+++ src/main/java/com/example/demo/service/TeacherService.java
@@ -3,5 +3,9 @@ package com.example.demo.service;
 import com.example.demo.model.Teacher;
 
 public interface TeacherService {
+    
+    /**
+     * saves teacher to database
+     */
     void add(Teacher teacher);
 }
