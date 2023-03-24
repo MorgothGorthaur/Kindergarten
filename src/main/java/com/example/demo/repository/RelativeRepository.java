08a01fae741877449b6857a20a4882fe28c2f9dd
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-24 18:42:53 +0200

    refactored

--- src/main/java/com/example/demo/repository/RelativeRepository.java
+++ src/main/java/com/example/demo/repository/RelativeRepository.java
@@ -9,15 +9,15 @@ import java.util.Optional;
 
 public interface RelativeRepository extends JpaRepository<Relative, Long> {
 
-    @Query("SELECT r FROM Relative r JOIN Child c JOIN c.group.teacher t WHERE r.id = ?1 AND c.id = ?2 AND t.email = ?3")
+    @Query("SELECT r FROM Relative r JOIN Child c WHERE r.id = ?1 AND c.id = ?2 AND c.group.teacher.email = ?3")
     Optional<Relative> findRelativeByIdAndChildIdAndTeacherEmail(long relativeId, long kidsId, String email);
 
-    @Query("SELECT r FROM Relative r JOIN r.kids k JOIN k.group.teacher t WHERE k.id = ?1 AND t.email = ?2")
+    @Query("SELECT r FROM Relative r JOIN Child c WHERE c.id = ?1 AND c.group.teacher.email = ?2")
     List<Relative> findRelativesByChildIdAndTeacherEmail(Long childId, String teacherEmail);
 
-    @Query("SELECT r FROM Relative r LEFT JOIN Child c WHERE r.name = ?1 AND r.address = ?2 AND r.phone = ?3")
+    @Query("SELECT r FROM Relative r WHERE r.name = ?1 AND r.address = ?2 AND r.phone = ?3")
     Optional<Relative> findEqualRelative(String name, String address, String phone);
 
-    @Query("SELECT r FROM Relative r LEFT JOIN Child c WHERE r.name = ?1 AND r.address = ?2 AND r.phone = ?3 AND r.id <> ?4")
+    @Query("SELECT r FROM Relative r WHERE r.name = ?1 AND r.address = ?2 AND r.phone = ?3 AND r.id <> ?4")
     Optional<Relative> findEqualRelativeWithAnotherId(String name, String address, String phone, long id);
 }
