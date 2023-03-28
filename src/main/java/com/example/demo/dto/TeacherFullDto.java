29012dc4ddb81e3383aea348459d45fd862d2362
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-03-28 16:17:45 +0300

    changed @Size

--- src/main/java/com/example/demo/dto/TeacherFullDto.java
+++ src/main/java/com/example/demo/dto/TeacherFullDto.java
@@ -4,10 +4,10 @@ import com.example.demo.model.Teacher;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
-public record TeacherFullDto(@Size(min = 1, max = 25, message = "name`s length must be between 1 and 25") String name,
+public record TeacherFullDto(@Size(min = 1, max = 35, message = "name`s length must be between 1 and 35") String name,
                              @Size(min = 1, max = 50, message = "phone`s length must be between 1 and 50") String phone,
-                             @Size(min = 1, max = 25, message = "skype`s length must be between 1 and 25") String skype,
-                             @Size(min = 1, max = 30, message = "email`s length must be between 1 and 30") @Email String email,
+                             @Size(min = 1, max = 50, message = "skype`s length must be between 1 and 50") String skype,
+                             @Size(min = 1, max = 50, message = "email`s length must be between 1 and 50") @Email String email,
                              @Size(min = 5, max = 15, message = "password`s length must be between 5 and 15") String password) {
     public Teacher createTeacher() {
         return new Teacher(name, phone, skype, email, password);
