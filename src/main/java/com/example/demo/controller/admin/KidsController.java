5090907986e9ca790ebe41eb2abb4237f3b53b6b
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-04-04 02:02:21 +0300

    refactored

--- src/main/java/com/example/demo/controller/admin/KidsController.java
+++ src/main/java/com/example/demo/controller/admin/KidsController.java
@@ -14,17 +14,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/kindergarten/admin/kids")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class KidsController {
     private final ChildRepository repository;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PatchMapping
     public void updateChild(@RequestBody @Valid ChildDto dto) {
         if (repository.updateChild(dto.id(), dto.name(), dto.birthYear()) == 0)
             throw new ChildNotFoundException(dto.id());
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public void deleteChild(@PathVariable long id) {
         if (repository.deleteById(id) == 0)
@@ -32,31 +31,27 @@ public class KidsController {
 
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/kids")
     public List<ChildWithGroupAndTeacherDto> get() {
         return repository.findAllWithGroupsAndTeachers().stream().map(ChildWithGroupAndTeacherDto::new).toList();
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/kids/group_name")
     public List<ChildWithGroupAndTeacherDto> getByGroupName() {
         return repository.findAllWithGroupsAndTeachersSortedByGroupName().stream().map(ChildWithGroupAndTeacherDto::new).toList();
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/kids/teacher_email")
     public List<ChildWithGroupAndTeacherDto> getByTeacherName() {
         return repository.findAllWithGroupsAndTeachersSortedByTeacherEmail().stream().map(ChildWithGroupAndTeacherDto::new).toList();
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/kids/name")
     public List<ChildWithGroupAndTeacherDto> getByName() {
         return repository.findAllWithGroupsAndTeachersSortedByName().stream().map(ChildWithGroupAndTeacherDto::new).toList();
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+
     @GetMapping("/kids/birth")
     public List<ChildWithGroupAndTeacherDto> getByBirth() {
         return repository.findAllWithGroupsAndTeachersSortedByBirth().stream().map(ChildWithGroupAndTeacherDto::new).toList();
