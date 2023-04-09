702718ae7ec4b233da68cf703037ca18b683b9c2
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-04-09 16:51:10 +0300

    removed custom methods

--- src/main/java/com/example/demo/repository/AccountRepository.java
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -2,20 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findAccountByEmail(String email);
-
-
-    @Modifying
-    @Query(value = """
-            UPDATE accounts a SET a.email = ?2, a.password = ?3 WHERE a.email = ?1
-            AND NOT EXISTS (SELECT a2.id FROM accounts a2 WHERE a2.email = ?2 AND a2.id <> a.id)
-            """, nativeQuery = true)
-    int updateAccountByEmail(String oldEmail, String newEmail, String newPassword);
 
 }
--- src/main/java/com/example/demo/repository/AdminRepository.java
+++ src/main/java/com/example/demo/repository/AdminRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Admin;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,21 +9,4 @@ public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     Optional<Admin> findAdminByEmail(String email);
 
-    @Modifying
-    @Query(value = """
-              UPDATE admins ad
-              JOIN accounts a ON ad.account_id = a.id
-              SET ad.phone_number = ?2
-              WHERE a.email = ?1
-            """, nativeQuery = true)
-    int updateAdminByEmail(String oldEmail, String newPhone);
-
-    @Modifying
-    @Transactional
-    @Query(value = """
-            DELETE admins FROM admins
-            JOIN accounts ON admins.account_id = accounts.id
-            WHERE accounts.email = ?1
-            """, nativeQuery = true)
-    int deleteAdminByEmail(String email);
 }
