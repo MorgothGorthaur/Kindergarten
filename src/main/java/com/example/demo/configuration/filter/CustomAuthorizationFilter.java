b3acb196f76b9fb103d06cc12a06afde0a0d7b61
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-09-27 01:05:15 +0300

    refactored, added order annotation

--- src/main/java/com/example/demo/configuration/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/example/demo/configuration/filter/CustomAuthorizationFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -14,6 +16,8 @@ import java.io.IOException;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RequiredArgsConstructor
+@Component
+@Order(3)
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
@@ -21,9 +25,13 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var authorizationHeader = request.getHeader(AUTHORIZATION);
-        if (authorizationHeader != null && authorizationHeader.startsWith(AuthorizationType.BEARER.getPrefix()))
+        if (containsBearerToken(authorizationHeader))
             tokenProvider.authorizeIfAccessTokenIsValid(authorizationHeader.substring(AuthorizationType.BEARER.getPrefix().length()));
         filterChain.doFilter(request, response);
+    }
+
+    private boolean containsBearerToken(String authorizationHeader) {
+        return authorizationHeader != null && authorizationHeader.startsWith(AuthorizationType.BEARER.getPrefix());
     }
 
 }
